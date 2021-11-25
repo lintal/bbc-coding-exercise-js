584a0412e83d6faa25c96fd3fe07d2363859aac0
Alex Lintott <alexander.lintott@bbc.co.uk>
2021-11-25 21:14:36 +0000

    feat: solution for exercise-1

--- index.js
+++ index.js
@@ -4,21 +4,43 @@ const moment = require('moment');
 const s3 = new AWS.S3();
 const dateFormat = 'ddd, DD MMM YYYY H:mm:ss z';
 
-const Bucket = ''; // How will you get the bucket name (environment variable: BUCKET_NAME)?
+const Bucket = process.env.BUCKET_NAME;
 
 exports.handler = async (event) => {
-  // event.Records[]; // Will contain a list of events from SQS that you need to process.
+  const { programme } = JSON.parse(
+    JSON.parse(
+      event.Records[0].body
+    ).Sns.Message
+  );
 
-  // You will need to get the current media-RSS document(s) from S3:
-  // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#getObject-property
-  // e.g.: s3.getObject({/* Parameters go here */}).promise();
+  const S3ObjectParams = {
+    Bucket,
+    Key: `${programme.parentPid}.json`,
+  };
 
-  // When manipulating dates, you can use the `moment` library which installed.
-  // https://momentjs.com/docs/
-  // The correct output format is also specified FOR YOU and can be achieved using:
-  // e.g. moment('// DATE STRING GOES HERE //').utc().format(dateFormat);
+  const feed = await s3.getObject(S3ObjectParams)
+    .promise()
+    .then((s3ObjectResponse) => JSON.parse(s3ObjectResponse.Body));
 
-  // You will need to store the resultant document(s) using:
-  // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#putObject-property
-  // e.g.: s3.putObject({/* Parameters go here */}).promise();
+  const programmeStart = moment(programme.versions[0].availability.dates.start).utc().format(dateFormat);
+
+  feed.channel.item.push({
+    title: programme.title,
+    description: programme.synopses.long,
+    guid: `urn:bbc:podcast:${programme.pid}`,
+    link: `https://www.bbc.co.uk/programmes/${programme.pid}`,
+    pubDate: programmeStart,
+    enclosure: {
+      type: 'audio/mpeg',
+      length: 0,
+      url: `https://example-programmes.bbc.co.uk/${programme.versions[0].pid}.mp3`
+    }
+  });
+
+  feed.channel.pubDate = programmeStart;
+
+  await s3.putObject({
+    ...S3ObjectParams,
+    Body: JSON.stringify(feed),
+  }).promise();
 }
