d0617a5f93f03710a2f02b200a6b20562152684d
Alex Lintott <alexander.lintott@bbc.co.uk>
2021-11-25 21:51:31 +0000

    feat: solution for exercise-2

--- index.js
+++ index.js
@@ -7,37 +7,53 @@ const dateFormat = 'ddd, DD MMM YYYY H:mm:ss z';
 const Bucket = process.env.BUCKET_NAME;
 
 exports.handler = async (event) => {
-  const { programme } = JSON.parse(
-    JSON.parse(
-      event.Records[0].body
-    ).Sns.Message
-  );
+  // Parse event data
+  const programmes = event.Records
+    .map((record) => JSON.parse(
+        JSON.parse(record.body).Sns.Message,
+      ).programme
+    );
 
   const S3ObjectParams = {
     Bucket,
-    Key: `${programme.parentPid}.json`,
+    Key: `${programmes[0].parentPid}.json`,
   };
 
   const feed = await s3.getObject(S3ObjectParams)
     .promise()
     .then((s3ObjectResponse) => JSON.parse(s3ObjectResponse.Body));
 
-  const programmeStart = moment(programme.versions[0].availability.dates.start).utc().format(dateFormat);
+  programmes.forEach((programme) => {
+    const programmeStart = moment(programme.versions[0].availability.dates.start).utc().format(dateFormat);
 
-  feed.channel.item.push({
-    title: programme.title,
-    description: programme.synopses.long,
-    guid: `urn:bbc:podcast:${programme.pid}`,
-    link: `https://www.bbc.co.uk/programmes/${programme.pid}`,
-    pubDate: programmeStart,
-    enclosure: {
-      type: 'audio/mpeg',
-      length: 0,
-      url: `https://example-programmes.bbc.co.uk/${programme.versions[0].pid}.mp3`
-    }
+    feed.channel.item.push({
+      title: programme.title,
+      description: programme.synopses.long,
+      guid: `urn:bbc:podcast:${programme.pid}`,
+      link: `https://www.bbc.co.uk/programmes/${programme.pid}`,
+      pubDate: programmeStart,
+      enclosure: {
+        type: 'audio/mpeg',
+        length: 0,
+        url: `https://example-programmes.bbc.co.uk/${programme.versions[0].pid}.mp3`
+      }
+    });
   });
 
-  feed.channel.pubDate = programmeStart;
+  const latest = programmes.sort((current, next) => {
+    const currentStart = moment(current.versions[0].availability.dates.start).utc();
+    const nextStart = moment(next.versions[0].availability.dates.start).utc();
+
+    if (currentStart.isBefore(nextStart)) {
+      return 1;
+    } else if (currentStart.isAfter(nextStart)) {
+      return -1;
+    }
+
+    return 0;
+  })[0];
+
+  feed.channel.pubDate = moment(latest.versions[0].availability.dates.start).utc().format(dateFormat);
 
   await s3.putObject({
     ...S3ObjectParams,
